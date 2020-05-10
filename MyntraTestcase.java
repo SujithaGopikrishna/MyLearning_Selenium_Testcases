dca8ed3b707beb10c8329b5c0c467431d58ea28e
Sujitha <63647569+SujithaGopikrishna@users.noreply.github.com>
2020-05-10 19:34:45 +0530

    Update MyntraTestcase.java

--- MyntraTestcase.java
+++ MyntraTestcase.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 
-public class Day1MyntraWorkout {
+public class MyntraTestcase {
 
 	public static void main(String[] args) throws InterruptedException {
 	System.setProperty("webdriver.chrome.driver", "./drivers./chromedriver.exe");
