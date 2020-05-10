9c565aef1ccde5730e8f64b2925f789308b6006f
Sujitha <63647569+SujithaGopikrishna@users.noreply.github.com>
2020-05-10 19:44:21 +0530

    Update and rename TC006BigBasketWorkout.java to BigBasketTestCase.java

--- BigBasketTestCase.java
+++ BigBasketTestCase.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class TC006BigBasketWorkout {
+public class BigBasketTestCase {
 
 	public static void main(String[] args) throws InterruptedException {
 		
@@ -28,20 +28,20 @@ public class TC006BigBasketWorkout {
 		System.setProperty("webdriver.chrome.silentouput", "true");
 			
 		
-		//Launch the URL: BigBasket Website
+//Launch the URL: BigBasket Website
 		ChromeDriver driver = new ChromeDriver(options);
 		driver.get("https://www.bigbasket.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		WebDriverWait wait = new WebDriverWait(driver,30);
 		
-		//Mouse over on the shop on category
+//Mouse over on the shop on category
 		Actions buildercategory = new Actions(driver);
 		WebElement category = driver.findElementByXPath("//a[text()=' Shop by Category ']");
 		buildercategory.moveToElement(category).perform();
 		Thread.sleep(2000);
 		
-		//Go to FOOD,GRAINS AND MASALA --> Rice,   //(//li[@data-submenu-id='foodgrains-oil-masala'])[1]/parent::ul ;;;;; //(//a[text()='Foodgrains, Oil & Masala'])[1]
+//Go to FOOD,GRAINS AND MASALA --> Rice,   //(//li[@data-submenu-id='foodgrains-oil-masala'])[1]/parent::ul ;;;;; //(//a[text()='Foodgrains, Oil & Masala'])[1]
 		
 		WebElement grains = driver.findElementByXPath("(//a[text()='Foodgrains, Oil & Masala'])[2]");
 		buildercategory.moveToElement(grains).perform();
@@ -53,16 +53,16 @@ public class TC006BigBasketWorkout {
 		
 		System.out.println("Rice and Rice product is selected");
 		
-		//Click on the Boiled and Steamed rice
+//Click on the Boiled and Steamed rice
 		
 		driver.findElementByXPath("(//a[text()='Boiled & Steam Rice'])[2]").click();
 		Thread.sleep(2000);
-		// Choose the brand bb Royal
+// Choose the brand bb Royal
 		
 		driver.findElementByXPath("(//span[text()='bb Royal'])[1]").click();
 	    System.out.println("Brand - bb Royal is choosed");
 		Thread.sleep(2000);
-	    // Go to Ponni Boiled Rice - Super Premium and select 5kg bag from Dropdown
+// Go to Ponni Boiled Rice - Super Premium and select 5kg bag from Dropdown
 	    
 	    Actions specificbrand = new Actions(driver);
 	    specificbrand.moveToElement(driver.findElementByXPath("//a[text()='Ponni Boiled Rice - Super Premium']")).perform();
@@ -75,13 +75,13 @@ public class TC006BigBasketWorkout {
 	    System.out.println("5kg is choosed");
 	    //li[@='allProducts in product.all_prods ']
 	    
-	    //Get the price of the product //(//div[@qa='price']//h4)[3]
+//Get the price of the product                     //(//div[@qa='price']//h4)[3]
 	    
 	    String Getprice = driver.findElementByXPath("(//span[@class='discnt-price'])[3]").getText().replaceAll("/D", "");
 	    System.out.println("Price of the selected Rice product:"+Getprice);
 	    Thread.sleep(2000);
 	    
-	    // Click on the "ADD"   //button[text()='Add '])[3]/parent::div
+ // Click on the "ADD"                     //button[text()='Add '])[3]/parent::div
 	    
 	    driver.findElementByXPath("(//button[@qa='add'])[3]//parent::div").click();
 	    Thread.sleep(2000);
@@ -94,22 +94,19 @@ public class TC006BigBasketWorkout {
 			System.out.println("Product delivery location detailes is not displayed ");
 			e.printStackTrace();
 		}
-		
-
-	    
-	    
+    
 	    // (//p[text()='You are viewing our product catalogue in'])[1]  //
 	    
 	    //String t1 = driver.findElementByXPath("(//p[text()='You are viewing our product catalogue in'])[1]//parent::div").getText();
 	    //System.out.println(t1);
 	    // (//p[text()=' to view your saved addresses']/a)[1] 
-//	    String t2= driver.findElementByXPath(" (//p[text()=' to view your saved addresses'])[1]").getText();
-//	    System.out.println(t2);
-	  //span[@class='arrow-marker']
-	   // Thread.sleep(2000);
-	   // WebElement locationpopup = driver.findElementByXPath("//span[@class='hvc']");
-	   // Actions builderpopup = new Actions(driver);
-	   // builderpopup.moveToElement(locationpopup);
+	    //String t2= driver.findElementByXPath(" (//p[text()=' to view your saved addresses'])[1]").getText();
+	    //System.out.println(t2);
+	    //span[@class='arrow-marker']
+	    // Thread.sleep(2000);
+	    // WebElement locationpopup = driver.findElementByXPath("//span[@class='hvc']");
+	    // Actions builderpopup = new Actions(driver);
+	    // builderpopup.moveToElement(locationpopup);
 	    // driver.findElementByXPath("//span[@class='arrow-marker']").click();
 	    //Thread.sleep(2000);
 	    
@@ -136,12 +133,12 @@ public class TC006BigBasketWorkout {
 	    //li[@ng-repeat='allProducts in product.all_prods '])[6]/parent::ul
 	    driver.findElementByXPath("(//li[@ng-repeat='allProducts in product.all_prods '])[5]").click();
 	    
-	    //print the price of the rice
+//print the price of the rice
 	    String getprice = driver.findElementByXPath("(//span[@class='discnt-price'])[2]").getText();
 	    System.out.println("Print the price of rice:" + getprice);
 	    Thread.sleep(1000);
 	    
-	    // Click on the Add button
+// Click on the Add button
 	    driver.findElementByXPath("(//button[@qa='add'])[2]").click();
 
 	    try {
@@ -155,9 +152,9 @@ public class TC006BigBasketWorkout {
 	    
 	    
 	    
-	   // driver.findElementByPartialLinkText("Successfully").click();
+	    // driver.findElementByPartialLinkText("Successfully").click();
 	    
-	    //Mouse over My Basket
+//Mouse over My Basket
 	    WebElement mybasket = driver.findElementByXPath("//span[text()='My Basket']");
 	    Actions builderbasket = new Actions(driver);
 	    builderbasket.moveToElement(mybasket).perform();
@@ -180,7 +177,7 @@ public class TC006BigBasketWorkout {
 	     System.out.println(sumoftwoitems);
 	    
          if (finalprice == sumoftwoitems)
-           {
+               {
    	    	System.out.println("Sum of all items price is correct");
 	       }
          
